fa121ecfa728749a736a8be051698fa8feaa2179
Saurabh Jaykar <saurabh@kali.kali.org>
2023-09-08 17:26:48 +0530

    pretier config added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-  <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  </>
+    <h1 className="text-3xl font-bold underline ">Hello world!</h1>
+  </>,
 );
-
-
