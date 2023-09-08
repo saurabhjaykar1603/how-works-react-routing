905003168603c198b2b17ba9f7d6a2b6fa239f18
Saurabh Jaykar <saurabh@kali.kali.org>
2023-09-08 17:14:52 +0530

    talwind setup

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
- 
+  <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   </>
 );
 
