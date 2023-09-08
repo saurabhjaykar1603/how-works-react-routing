e4d39b42c35581778bd1c68ab17ed4108af6f4c5
Saurabh Jaykar <saurabh@kali.kali.org>
2023-09-09 00:41:40 +0530

    window obj path change

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
+/* eslint-disable default-case */
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import About from "./view/About/About";
 import Contact from "./view/Contact/Contact";
 import Home from "./view/Home/Home";
-function App() {
-  return (
-    <div className="App">
-      <Home/>
-      <About/>
-      <Contact/>
-    </div>
-  );
-}
 
+function App() {
+  const path = window.location.pathname;
+
+  switch (path) {
+    case "/":
+      return <Home />;
+      case "/About":
+        return <About/>;
+        case "/Contact":
+          return <Contact/>;
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-  <App/>
-
-  </>
+    <App />
+  </>,
 );
